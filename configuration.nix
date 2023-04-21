0ee9d3bef470852b80b7e6fa54c00ed92866ceaf
deuce <aj.doucett@gmail.com>
2023-04-21 18:27:08 -0400

    g

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,12 @@
   boot.blacklistedKernelModules = [ "radeon" ];
 
   networking.hostName = "Alpha"; # Define your hostname.
- 
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -40,6 +45,19 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  # Enable the X11 windowing system.
+#  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+ # services.xserver.displayManager.gdm.enable = true;
+ # services.xserver.desktopManager.gnome.enable = true;
+ # services.xserver.windowManager.bspwm.enable = true;
+
+# Configure keymap in X11
+#  services.xserver = {
+#    layout = "us";
+#    xkbVariant = "";
+#  };
 
 
 services = {
@@ -60,6 +78,21 @@ services = {
       xkbVariant = "";
           };
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -75,6 +108,7 @@ services = {
       opengl.extraPackages32 = with pkgs; [ ];
     };  
 
+
   # Enable Xbox support
   hardware.xone.enable = true;
 
@@ -88,10 +122,16 @@ services = {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
   };
 
-
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.deuce = {
@@ -298,6 +338,36 @@ services = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -305,7 +375,49 @@ services = {
   #  wget
   ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  #  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
 }
