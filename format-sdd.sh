ad3a32aa35e25ff680ac4508450b7d32e7be521f
deuceaj <43860830+deuceaj@users.noreply.github.com>
2023-07-05 12:32:00 -0400

    Update format-sdd.sh

--- format-sdd.sh
+++ format-sdd.sh
@@ -1,22 +1,22 @@
 # Create Partition Table
-parted /dev/sda -- mklabel gpt
+parted /dev/nvme0n1 -- mklabel gpt
 
 # Create Partitions
-parted /dev/sda -- mkpart primary 512MiB -8GiB
-parted /dev/sda -- mkpart primary linux-swap -8GiB 100%
-parted /dev/sda -- mkpart ESP fat32 1Mib 512MiB
-parted /dev/sda -- set 3 esp on
+parted /dev/nvme0n1 -- mkpart primary 512MiB -8GiB
+parted /dev/nvme0n1 -- mkpart primary linux-swap -8GiB 100%
+parted /dev/nvme0n1 -- mkpart ESP fat32 1Mib 512MiB
+parted /dev/nvme0n1 -- set 3 esp on
 
 # Define File System
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3
+mkfs.ext4 -L nixos /dev/nvme0n1p1
+mkswap -L swap /dev/nvme0n1p2
+mkfs.fat -F 32 -n boot /dev/nvme0n1p3
 
 # Mount Partitions
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/disk/by-label/boot /mnt/boot/efi
-swapon /dev/sda2
+swapon /dev/nvme0n1p2
 
 # Generate Nixos Config file
 nixos-generate-config --root /mnt
